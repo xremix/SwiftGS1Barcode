80e88fd7638cc8694fdfd5d95e7c8ff4e082350e
thoffmann <toni.hoffmann@arthrex.de>
2017-06-26 11:18:04 +0200

    Init Project commit

---
+++ SwiftGS1Barcode/SwiftGS1Barcode.h
@@ -0,0 +1,19 @@
+//
+//  SwiftGS1Barcode.h
+//  SwiftGS1Barcode
+//
+//  Created by Toni Hoffmann on 26.06.17.
+//  Copyright © 2017 Toni Hoffmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftGS1Barcode.
+FOUNDATION_EXPORT double SwiftGS1BarcodeVersionNumber;
+
+//! Project version string for SwiftGS1Barcode.
+FOUNDATION_EXPORT const unsigned char SwiftGS1BarcodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftGS1Barcode/PublicHeader.h>
+
+
