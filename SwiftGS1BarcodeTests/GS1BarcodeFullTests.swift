5214fdee454fc0b241aad3200197b689736c8253
xremix <tonidhoffmann@gmail.com>
2020-02-03 08:36:31 +0100

    renaming tests

--- SwiftGS1BarcodeTests/GS1BarcodeFullTests.swift
+++ SwiftGS1BarcodeTests/GS1BarcodeFullTests.swift
@@ -9,18 +9,18 @@
 import XCTest
 @testable import SwiftGS1Barcode
 
-class GS1BarcodeSampleTests: XCTestCase {
-    
-    func testSample1(){
+class GS1BarcodeFullTests: XCTestCase {
+
+    func testFullSample1(){
         // 01 17 10 21
         let barcode = GS1Barcode(raw: "01006141410073491714123110A1234B\u{1D}211234")
         XCTAssertEqual(barcode.gtin, "00614141007349")
         XCTAssertEqual(barcode.expirationDate, Date.from(year: 2014, month: 12, day: 31))
         XCTAssertEqual(barcode.lotNumber, "A1234B")
         XCTAssertEqual(barcode.serialNumber, "1234")
-        
+
     }
-    func testSample2(){
+    func testFullSample2(){
         // 01 11 10 21
         let barcode = GS1Barcode(raw: "0112345678900101\u{1D}110809061002\u{1D}21027-32")
         XCTAssertEqual(barcode.gtin, "12345678900101")
@@ -28,16 +28,14 @@ class GS1BarcodeSampleTests: XCTestCase {
         XCTAssertEqual(barcode.lotNumber, "02")
         XCTAssertEqual(barcode.serialNumber, "027-32")
         XCTAssert(barcode.validate())
-        
+
     }
-    func testSample3(){
+    func testFullSample3(){
         // 01 3101 10
         // 01 97350053850012 310 1 000050 10 897A174
         let barcode = GS1Barcode(raw: "0197350053850012310100005010897A174")
         XCTAssertEqual(barcode.gtin, "97350053850012")
         XCTAssertEqual(barcode.lotNumber, "897A174")
         XCTAssert(barcode.validate())
-        
     }
-    
 }
